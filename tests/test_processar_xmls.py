f3f6ff1f49cf8bb512217edff3dd4adc5d3785f6
Gms006 <gptjoao01@gmail.com>
2025-07-28 16:33:17 -0300

    Add XML key column and reorder output

--- tests/test_processar_xmls.py
+++ tests/test_processar_xmls.py
@@ -14,7 +14,7 @@ def test_processar_xmls_uses_configurador(monkeypatch):
     def fake_extrair(_):
         return [{
             "Emitente CNPJ": "111",
-            "Destinatario CNPJ": "222",
+            "Destinatário CNPJ": "222",
             "CFOP": "1102",
             "Chassi": "ABCDEFGH123456789",
             "Data Emissão": pd.Timestamp("2023-01-01")
@@ -73,5 +73,50 @@ def test_processar_xmls_filters_columns(tmp_path):
     xml_file = tmp_path / "nota.xml"
     xml_file.write_text(xml_content, encoding="utf-8")
     df = ev.processar_xmls([str(xml_file)], "12345678000199")
-    assert list(df.columns) == list(ev.LAYOUT_COLUNAS.keys())
+
+    ordered = sorted(ev.LAYOUT_COLUNAS.items(), key=lambda x: x[1]["ordem"])
+    base_cols = [c for c, _ in ordered]
+    extras = ["Empresa CNPJ", "Tipo Produto", "Mês Emissão"]
+    expected_cols = [
+        "Tipo Nota",
+        *[c for c in base_cols if c not in {"Tipo Nota", "Natureza Operação", "CHAVE XML"}],
+        *extras,
+        "Natureza Operação",
+        "CHAVE XML",
+    ]
+
+    assert list(df.columns) == expected_cols
+
+
+def test_mes_emissao_format(tmp_path):
+    xml_content = '''<?xml version="1.0" encoding="UTF-8"?>
+    <NFe xmlns="http://www.portalfiscal.inf.br/nfe">
+        <infNFe Id="NFe001" versao="4.00">
+            <ide>
+                <nNF>1</nNF>
+                <dhEmi>2023-07-15T00:00:00-03:00</dhEmi>
+                <tpNF>1</tpNF>
+            </ide>
+            <emit>
+                <CNPJ>12345678000199</CNPJ>
+                <xNome>Emitente</xNome>
+            </emit>
+            <dest>
+                <CNPJ>98765432000188</CNPJ>
+                <xNome>Destinatario</xNome>
+            </dest>
+            <det nItem="1">
+                <prod>
+                    <xProd>CARRO XYZ</xProd>
+                    <CFOP>5102</CFOP>
+                    <vProd>1000.00</vProd>
+                </prod>
+            </det>
+        </infNFe>
+    </NFe>'''
+    xml_file = tmp_path / "nota.xml"
+    xml_file.write_text(xml_content, encoding="utf-8")
+
+    df = ev.processar_xmls([str(xml_file)], "12345678000199")
+    assert df.loc[0, "Mês Emissão"] == "07/2023"
 
